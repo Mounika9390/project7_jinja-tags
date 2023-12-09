d534811ab5ab08ca8b801fc44745bd087bb3777f
Mounika9390 <mmounika9390@gmail.com>
2023-12-09 22:43:37 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+
+# Create your views here.
+def Anusha(request):
+    return render(request,'Anusha.html')
+def Mounika(request):
+    return render(request,'Mounika.html')
